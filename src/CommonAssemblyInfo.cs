d2a7de415a4d10fb5516dae7310fc29b96bf81f2
jonnii <me@jonnii.com>
2012-08-17 10:53:16 -0400

    add license, common assembly info, version file

---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("Chinchilla, not just a furry animal.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("http://github.com/jonnii/chinchilla")]
+[assembly: AssemblyProduct("Chinchilla")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("0.1.4")]
+[assembly: AssemblyFileVersion("0.1.4")]
