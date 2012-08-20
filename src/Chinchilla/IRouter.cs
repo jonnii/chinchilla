f1a302422d9886adbcb5e96398338318c8677824
jonnii <me@jonnii.com>
2012-08-20 10:22:52 -0400

    docs

--- src/Chinchilla/IRouter.cs
+++ src/Chinchilla/IRouter.cs
@@ -1,7 +1,13 @@
 namespace Chinchilla
 {
+    /// <summary>
+    /// A router is responsible for creating a routing key for a message
+    /// </summary>
     public interface IRouter
     {
+        /// <summary>
+        /// Gets a routing key for a given message
+        /// </summary>
         string Route<TMessage>(TMessage message);
     }
 }
