3935cbd0092a593c01aadb9e6c4b77d17a9b2b86
jonnii <me@jonnii.com>
2012-09-11 12:06:22 -0400

    more equality members.

--- src/Chinchilla.Api/Permission.cs
+++ src/Chinchilla.Api/Permission.cs
@@ -21,5 +21,38 @@ namespace Chinchilla.Api
         public string Write { get; set; }
 
         public string Read { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            return obj.GetType() == GetType() && Equals((Permission)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = Configure != null ? Configure.GetHashCode() : 0;
+                hashCode = (hashCode * 397) ^ (Write != null ? Write.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (Read != null ? Read.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
+
+        protected bool Equals(Permission other)
+        {
+            return string.Equals(Configure, other.Configure)
+                   && string.Equals(Write, other.Write)
+                   && string.Equals(Read, other.Read);
+        }
     }
 }
--- src/Chinchilla.Api/Permissions.cs
+++ src/Chinchilla.Api/Permissions.cs
@@ -5,5 +5,36 @@ namespace Chinchilla.Api
         public string Vhost { get; set; }
 
         public string User { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            return obj.GetType() == GetType() && Equals((Permissions)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = base.GetHashCode();
+                hashCode = (hashCode * 397) ^ (Vhost != null ? Vhost.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (User != null ? User.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
+
+        protected bool Equals(Permissions other)
+        {
+            return base.Equals(other) && string.Equals(Vhost, other.Vhost) && string.Equals(User, other.User);
+        }
     }
 }
