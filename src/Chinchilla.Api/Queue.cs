d85ad993ef5d4e2b56cd523ff3ea4acf43a1de2f
jonnii <me@jonnii.com>
2012-09-11 11:56:40 -0400

    generate some equality members

--- src/Chinchilla.Api/Queue.cs
+++ src/Chinchilla.Api/Queue.cs
@@ -10,5 +10,30 @@ namespace Chinchilla.Api
         }
 
         public string Name { get; set; }
+
+        protected bool Equals(Queue other)
+        {
+            return string.Equals(Name, other.Name);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            return obj.GetType() == GetType() && Equals((Queue)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Name != null ? Name.GetHashCode() : 0;
+        }
     }
 }
--- src/Chinchilla.Api/User.cs
+++ src/Chinchilla.Api/User.cs
@@ -10,5 +10,30 @@ namespace Chinchilla.Api
         }
 
         public string Name { get; set; }
+
+        protected bool Equals(User other)
+        {
+            return string.Equals(Name, other.Name);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            return obj.GetType() == GetType() && Equals((User)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Name != null ? Name.GetHashCode() : 0;
+        }
     }
 }
