2ead96717b70cd184ce4acb162bc887ee74403cf
jonnii <me@jonnii.com>
2012-09-11 12:00:47 -0400

    formatting.

--- src/Chinchilla.Api/Message.cs
+++ src/Chinchilla.Api/Message.cs
@@ -3,11 +3,17 @@
     public class Message
     {
         public int PayloadBytes { get; set; }
+
         public bool Redelivered { get; set; }
+
         public string Exchange { get; set; }
+
         public string RoutingKey { get; set; }
+
         public int MessageCount { get; set; }
+
         public MessageProperties MessageProperties { get; set; }
+
         public string Payload { get; set; }
     }
 }
