3f12f11a2dad900d0790fac159df9f89d49030cd
Jonathan Goldman <me@jonnii.com>
2013-06-03 05:35:30 -0400

    cache typed serializer instances

--- src/Chinchilla.Serializers.MsgPack/MessagePackMessageSerializer.cs
+++ src/Chinchilla.Serializers.MsgPack/MessagePackMessageSerializer.cs
@@ -4,6 +4,12 @@ namespace Chinchilla.Serializers.MsgPack
 {
     public class MessagePackMessageSerializer : IMessageSerializer
     {
+        public static class SerializerInstance<T>
+        {
+            public static readonly MessagePackSerializer<MessageWrapper<T>> CachedInstance =
+                MessagePackSerializer.Create<MessageWrapper<T>>();
+        }
+
         public class MessageWrapper<T>
         {
             public T Body { get; set; }
@@ -16,13 +22,13 @@ namespace Chinchilla.Serializers.MsgPack
 
         public byte[] Serialize<T>(IMessage<T> message)
         {
-            var packer = MessagePackSerializer.Create<MessageWrapper<T>>();
+            var packer = SerializerInstance<T>.CachedInstance;
             return packer.PackSingleObject(new MessageWrapper<T> { Body = message.Body });
         }
 
         public IMessage<T> Deserialize<T>(byte[] message)
         {
-            var packer = MessagePackSerializer.Create<MessageWrapper<T>>();
+            var packer = SerializerInstance<T>.CachedInstance;
             var wrapped = packer.UnpackSingleObject(message);
             return new Message<T>(wrapped.Body);
         }
