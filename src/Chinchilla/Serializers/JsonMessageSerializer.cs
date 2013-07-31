4592eeab2cb6d6a9c80c0561ab322eec3739731c
jonnii <me@jonnii.com>
2013-07-31 13:57:13 -0400

    Remove json.net from main chinchilla library.

--- src/Chinchilla/Serializers/JsonMessageSerializer.cs
+++ src/Chinchilla/Serializers/JsonMessageSerializer.cs
@@ -1,5 +1,4 @@
 ﻿using System.Text;
-using Newtonsoft.Json;
 
 namespace Chinchilla.Serializers
 {
@@ -12,12 +11,14 @@ namespace Chinchilla.Serializers
 
         public byte[] Serialize<T>(IMessage<T> message)
         {
-            return Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+            var serialized = SimpleJson.SerializeObject(message);
+            return Encoding.UTF8.GetBytes(serialized);
         }
 
         public IMessage<T> Deserialize<T>(byte[] message)
         {
-            return JsonConvert.DeserializeObject<Message<T>>(Encoding.UTF8.GetString(message));
+            var decoded = Encoding.UTF8.GetString(message);
+            return SimpleJson.DeserializeObject<Message<T>>(decoded);
         }
     }
 }
