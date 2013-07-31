9eae3469d061b9b54ead4d7ce2c035019ec2a43c
jonnii <me@jonnii.com>
2013-07-31 14:09:40 -0400

    add serializer project for json.net

---
+++ src/Chinchilla.Serializers.JsonNET.Specifications/JsonNetMessageSerializerSpecification.cs
@@ -0,0 +1,58 @@
+﻿using Machine.Fakes;
+using Machine.Specifications;
+
+namespace Chinchilla.Serializers.JsonNET.Specifications
+{
+    public class JsonNetMessageSerializerSpecification
+    {
+        [Subject(typeof(JsonMessageSerializer))]
+        public class in_general : WithSubject<JsonNetMessageSerializer>
+        {
+            It should_have_content_type = () =>
+                Subject.ContentType.ShouldEqual("application/json");
+        }
+
+        [Subject(typeof(JsonMessageSerializer))]
+        public class when_serializing : WithSubject<JsonMessageSerializer>
+        {
+            Because of = () =>
+                serialized = Subject.Serialize(Message.Create(new InterestingFact()));
+
+            It should_serialize = () =>
+                serialized.Length.ShouldBeGreaterThan(0);
+
+            static byte[] serialized;
+        }
+
+        [Subject(typeof(JsonMessageSerializer))]
+        public class when_deserializing : WithSubject<JsonMessageSerializer>
+        {
+            Establish context = () =>
+                serialized = Subject.Serialize(
+                    Message.Create(new InterestingFact("Disney's Tangled is the 3rd most expensive film ever made...")));
+
+            Because of = () =>
+                deserialized = Subject.Deserialize<InterestingFact>(serialized);
+
+            It should_deserialize = () =>
+                deserialized.Body.FactBody.ShouldEqual("Disney's Tangled is the 3rd most expensive film ever made...");
+
+            static byte[] serialized;
+
+            static IMessage<InterestingFact> deserialized;
+        }
+
+        public class InterestingFact
+        {
+            public InterestingFact() { }
+
+            public InterestingFact(string body)
+            {
+                FactBody = body;
+            }
+
+            public string FactBody { get; set; }
+        }
+    }
+
+}
