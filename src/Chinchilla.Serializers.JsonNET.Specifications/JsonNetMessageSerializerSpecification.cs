9f16d48998ed1bd27a0746840a20fc66bd376083
Jon Radchenko <jon.radchenko@mlp.com>
2016-03-23 17:31:48 -0400

    [Feature] Add settings option for json net serializer

--- src/Chinchilla.Serializers.JsonNET.Specifications/JsonNetMessageSerializerSpecification.cs
+++ src/Chinchilla.Serializers.JsonNET.Specifications/JsonNetMessageSerializerSpecification.cs
@@ -1,19 +1,21 @@
-﻿using Machine.Fakes;
+﻿using System;
+using Machine.Fakes;
 using Machine.Specifications;
+using Newtonsoft.Json;
 
 namespace Chinchilla.Serializers.JsonNET.Specifications
 {
     public class JsonNetMessageSerializerSpecification
     {
-        [Subject(typeof(JsonMessageSerializer))]
+        [Subject(typeof(JsonNetMessageSerializer))]
         public class in_general : WithSubject<JsonNetMessageSerializer>
         {
             It should_have_content_type = () =>
                 Subject.ContentType.ShouldEqual("application/json");
         }
 
-        [Subject(typeof(JsonMessageSerializer))]
-        public class when_serializing : WithSubject<JsonMessageSerializer>
+        [Subject(typeof(JsonNetMessageSerializer))]
+        public class when_serializing : WithSubject<JsonNetMessageSerializer>
         {
             Because of = () =>
                 serialized = Subject.Serialize(Message.Create(new InterestingFact()));
@@ -24,8 +26,8 @@ namespace Chinchilla.Serializers.JsonNET.Specifications
             static byte[] serialized;
         }
 
-        [Subject(typeof(JsonMessageSerializer))]
-        public class when_deserializing : WithSubject<JsonMessageSerializer>
+        [Subject(typeof(JsonNetMessageSerializer))]
+        public class when_deserializing : WithSubject<JsonNetMessageSerializer>
         {
             Establish context = () =>
                 serialized = Subject.Serialize(
@@ -40,6 +42,48 @@ namespace Chinchilla.Serializers.JsonNET.Specifications
             static byte[] serialized;
 
             static IMessage<InterestingFact> deserialized;
+        }
+
+        [Subject(typeof(JsonNetMessageSerializer))]
+        public class when_deserializing_with_settings
+        {
+            Establish context = () =>
+            {
+                Subject = new JsonNetMessageSerializer(new JsonSerializerSettings
+                {
+                    MissingMemberHandling = MissingMemberHandling.Error
+                });
+
+                serialized = Subject.Serialize(
+                    Message.Create(new ReallyInterestingFact(
+                        "Disney's Tangled is the 3rd most expensive film ever made...",
+                        "Tangled was Disney’s first full-length computer-animated fairy-tale adventure")));
+            };
+
+            Because of = () =>
+                exception = Catch.Exception(() => Subject.Deserialize<InterestingFact>(serialized));
+
+            It should_respect_settings_and_throw = () =>
+                exception.ShouldBeOfExactType<JsonSerializationException>();
+
+            static JsonNetMessageSerializer Subject;
+
+            static byte[] serialized;
+
+            static Exception exception;
+        }
+
+        public class ReallyInterestingFact : InterestingFact
+        {
+            public ReallyInterestingFact() { }
+
+            public ReallyInterestingFact(string body, string reallyInterestingBody)
+                : base(body)
+            {
+                FactReallyInterestingBody = reallyInterestingBody;
+            }
+
+            public string FactReallyInterestingBody { get; set; }
         }
 
         public class InterestingFact
