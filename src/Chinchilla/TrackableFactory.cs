775fecd1818872ba8939225efcfab50722f1920c
bieri <k.bieri@allegrodevelopment.com>
2016-04-04 20:27:25 -0500

    Amend the TrackableFactory to use a ConcurrentDictionary under the covers to prevent the Thread Access issues found in testing.

--- src/Chinchilla/TrackableFactory.cs
+++ src/Chinchilla/TrackableFactory.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,27 +8,27 @@ namespace Chinchilla
     public abstract class TrackableFactory<TTrackable> : IDisposable
         where TTrackable : Trackable
     {
-        private readonly List<TTrackable> trackables = new List<TTrackable>();
+        private readonly ConcurrentDictionary<int, TTrackable> trackables = new ConcurrentDictionary<int, TTrackable>();
 
         public IEnumerable<TTrackable> Tracked
         {
-            get { return trackables; }
+            get { return trackables.Values; }
         }
 
         public bool IsTracking(TTrackable trackable)
         {
-            return trackables.Contains(trackable);
+            return trackables.ContainsKey(trackable.GetHashCode());
         }
 
         protected void Track(TTrackable trackable)
         {
             trackable.Disposed += Untrack;
-            trackables.Add(trackable);
+            trackables.TryAdd(trackable.GetHashCode(), trackable);
         }
 
         private void Untrack(TTrackable trackable)
         {
-            trackables.Remove(trackable);
+            trackables.TryRemove(trackable.GetHashCode(), out trackable);
         }
 
         private void Untrack(object sender, EventArgs eventArgs)
