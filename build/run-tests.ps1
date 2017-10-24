cc00c685fac43f8b4c56510106c3f6977afbc224
Robert Coltheart <robertcoltheart@users.noreply.github.com>
2017-10-24 08:06:39 +0800

    [Feature] Publish artifacts to nuget via appveyor (#92)
    * [Feature] Publish artifacts to nuget via appveyor
    
    * Just use appveyor
    
    * Fixes
    
    * fix
    
    * fix
    
    * fix tests
    
    * fix tests
    
    * fix tests
    
    * tidy
    
    * More fixes
    
    * versioning

---
+++ build/run-tests.ps1
@@ -0,0 +1,9 @@
+$testExitCode = 0
+
+Get-ChildItem src\*.Specifications `
+| select -ExpandProperty Name `
+| % {
+	dotnet test "src\\$_" -c Release --no-build --logger trx --results-directory (Join-Path $(pwd) test_results)
+	$testExitCode += $LASTEXITCODE
+}
+if ($testExitCode -ne 0) { $host.SetShouldExit($testExitCode); throw; }
