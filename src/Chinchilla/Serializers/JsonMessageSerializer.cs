c96e9438f25218e2eed144963e684c07361e8659
Jonathan Goldman <me@jonnii.com>
2017-11-13 22:39:41 -0500

    use newtonsoft in main library

--- src/Chinchilla/Serializers/JsonMessageSerializer.cs
+++ src/Chinchilla/Serializers/JsonMessageSerializer.cs
@@ -1,10 +1,11 @@
 ﻿using System.Text;
+using Newtonsoft.Json;
 
 namespace Chinchilla.Serializers
 {
     public class JsonMessageSerializer : IMessageSerializer
     {
-        private readonly IJsonSerializerStrategy strategy;
+        //private readonly IJsonSerializerStrategy strategy;
 
         public JsonMessageSerializer()
             : this(new DefaultMessageTypeFactory())
@@ -13,21 +14,21 @@ namespace Chinchilla.Serializers
 
         public JsonMessageSerializer(IMessageTypeFactory messageTypeFactory)
         {
-            strategy = new ChinchillaSerializerStrategy(messageTypeFactory);
+            //strategy = new ChinchillaSerializerStrategy(messageTypeFactory);
         }
 
         public string ContentType { get; } = "application/json";
 
         public byte[] Serialize<T>(IMessage<T> message)
         {
-            var serialized = SimpleJson.SerializeObject(message.Body, strategy);
+            var serialized = JsonConvert.SerializeObject(message.Body);
             return Encoding.UTF8.GetBytes(serialized);
         }
 
         public IMessage<T> Deserialize<T>(byte[] message)
         {
             var decoded = Encoding.UTF8.GetString(message);
-            var deserialized = SimpleJson.DeserializeObject<T>(decoded, strategy);
+            var deserialized = JsonConvert.DeserializeObject<T>(decoded);
             return Message.Create(deserialized);
         }
     }
