d1d7f2962b3f04aa3cab9e05a1a1374e59401410
Jonathan Goldman <me@jonnii.com>
2017-11-13 23:19:38 -0500

    Also run integration tests

--- build/run-tests.ps1
+++ build/run-tests.ps1
@@ -6,4 +6,12 @@ Get-ChildItem src\*.Specifications `
 	dotnet test "src\\$_" -c Release --no-build --logger trx --results-directory (Join-Path $(pwd) test_results)
 	$testExitCode += $LASTEXITCODE
 }
+
+Get-ChildItem src\*.Integration `
+| select -ExpandProperty Name `
+| % {
+	dotnet test "src\\$_" -c Release --no-build --logger trx --results-directory (Join-Path $(pwd) test_results)
+	$testExitCode += $LASTEXITCODE
+}
+
 if ($testExitCode -ne 0) { $host.SetShouldExit($testExitCode); throw; }
