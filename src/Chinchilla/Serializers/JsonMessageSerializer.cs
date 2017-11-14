b4c85be21cf09940504f7283c5c3887703afc3c4
Jonathan Goldman <me@jonnii.com>
2017-11-13 23:03:58 -0500

    ability to create interfaces

--- src/Chinchilla/Serializers/JsonMessageSerializer.cs
+++ src/Chinchilla/Serializers/JsonMessageSerializer.cs
@@ -1,12 +1,15 @@
-﻿using System.Text;
+﻿using System;
+using System.Reflection;
+using System.Text;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
 
 namespace Chinchilla.Serializers
 {
     public class JsonMessageSerializer : IMessageSerializer
     {
-        //private readonly IJsonSerializerStrategy strategy;
+        private readonly JsonSerializerSettings settings;
 
         public JsonMessageSerializer()
             : this(new DefaultMessageTypeFactory())
@@ -15,17 +18,40 @@ namespace Chinchilla.Serializers
 
         public JsonMessageSerializer(IMessageTypeFactory messageTypeFactory)
         {
-            //strategy = new ChinchillaSerializerStrategy(messageTypeFactory);
+            settings = new JsonSerializerSettings{
+                Converters = new []{ new StringEnumConverter() },
+                ContractResolver = new MagicContractResolver(messageTypeFactory)
+            };
+        }
+
+        public class MagicContractResolver : DefaultContractResolver
+        {
+            private readonly IMessageTypeFactory messageTypeFactory;
+
+            public MagicContractResolver(IMessageTypeFactory messageTypeFactory)
+            {
+                this.messageTypeFactory = messageTypeFactory;
+            }
+
+            protected override JsonObjectContract CreateObjectContract(Type objectType)
+            {
+                JsonObjectContract contract = base.CreateObjectContract(objectType);
+
+                if (!objectType.GetTypeInfo().IsInterface)
+                {
+                    return contract;
+                }
+
+                contract.DefaultCreator = messageTypeFactory.GetTypeFactory(objectType);
+
+                return contract;
+            }
         }
 
         public string ContentType { get; } = "application/json";
 
         public byte[] Serialize<T>(IMessage<T> message)
         {
-            var settings = new JsonSerializerSettings{
-                Converters = new []{new StringEnumConverter()}
-            };
-            
             var serialized = JsonConvert.SerializeObject(message.Body, settings);
             return Encoding.UTF8.GetBytes(serialized);
         }
@@ -33,7 +59,7 @@ namespace Chinchilla.Serializers
         public IMessage<T> Deserialize<T>(byte[] message)
         {
             var decoded = Encoding.UTF8.GetString(message);
-            var deserialized = JsonConvert.DeserializeObject<T>(decoded);
+            var deserialized = JsonConvert.DeserializeObject<T>(decoded, settings);
             return Message.Create(deserialized);
         }
     }
